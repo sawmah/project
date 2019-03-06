2ee25e89f464e2f7f44b4efffb37cdc66b8ade7b
Sawmah <quentinzho@gmail.com>
2019-03-06 15:36:54 +0800

    homework

--- routes.js
+++ routes.js
@@ -8,17 +8,18 @@ var appRouter = function (app) {
     res.status(200).send({ message: 'Welcome to our restful API' });
   });
 
-  app.get("/user", function (req, res) {
+  app.get("/student", function (req, res) {
     var data = ({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       username: faker.internet.userName(),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      id: faker.random.uuid()
     });
     res.status(200).send(data);
   });
 
- app.get("/users/:num", function (req, res) {
+ app.get("/students/:num", function (req, res) {
    var users = [];
    var num = req.params.num;
 
@@ -28,7 +29,8 @@ var appRouter = function (app) {
            firstName: faker.name.firstName(),
            lastName: faker.name.lastName(),
            username: faker.internet.userName(),
-           email: faker.internet.email()
+           email: faker.internet.email(),
+           id: faker.random.uuid()
         });
      }
 
